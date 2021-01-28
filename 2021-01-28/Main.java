b14e00fff27f4c3fb022a55055734041f1e06ea2
Andy Olsen <andyo@olsensoft.com>
2021-01-28 19:16:05 +0000

    Update Main.java

--- 2021-01-28/Main.java
+++ 2021-01-28/Main.java
@@ -3,6 +3,7 @@ package no.dnb.OnlineRetailer;
 
 public class Main {
     public static void main(String[] args) {
+        // ANDY: YES EXACTLY RIGHT :-D
         Menu usermenu = new Menu();
         usermenu.menuStart();
     }
