4cd364a5754669c673233f52fd4e377463c29510
svetlabe <74042787+svetlabe@users.noreply.github.com>
2021-01-29 16:21:13 +0100

    Update Main.java

--- 2021-01-29/Main.java
+++ 2021-01-29/Main.java
@@ -1,1 +1,10 @@
+package no.dnb.OnlineRetailer;
 
+
+public class Main {
+    public static void main(String[] args) {
+        Menu usermenu = new Menu();
+        usermenu.menuStart();
+    }
+
+}
