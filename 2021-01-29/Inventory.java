d37808ced0286aef841ae6b763969f27d2c333ef
svetlabe <74042787+svetlabe@users.noreply.github.com>
2021-02-03 11:04:19 +0100

    Update Inventory.java

--- 2021-01-29/Inventory.java
+++ 2021-01-29/Inventory.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 
 public class Inventory {
 
-    private  ArrayList<Product> products = new ArrayList<Product>();
+    private  ArrayList<Product> products = new ArrayList<>();
 
 // getters and setters
 
